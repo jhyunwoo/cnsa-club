f9c64a05074bd525d6872d7d3e46fb8015a31ae1
Hyunwoo <88994338+jhyunwoo@users.noreply.github.com>
2023-02-12 11:48:02 +0900

    Update SubmitAlert.tsx

--- components/SubmitAlert.tsx
+++ components/SubmitAlert.tsx
@@ -1,6 +1,11 @@
 import { useRouter } from 'next/router'
 
-export default function SubmitAlert(props:string) {
+
+interface Iprops {
+    title: string,
+}
+
+export default function SubmitAlert(props:Iprops) {
   const router = useRouter()
   return (
     <div className="fixed top-0 right-0 left-0 bg-slate-100/50 flex flex-col justify-center items-center w-screen h-screen backdrop-blur-sm">
