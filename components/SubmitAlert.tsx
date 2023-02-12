d28454d5969e475a968b900a5349ecab91832c91
Hyunwoo <88994338+jhyunwoo@users.noreply.github.com>
2023-02-12 11:40:13 +0900

    Update SubmitAlert.tsx

--- components/SubmitAlert.tsx
+++ components/SubmitAlert.tsx
@@ -1,10 +1,10 @@
 import { useRouter } from 'next/router'
 
-export default function SubmitAlert({title}:String) {
+export default function SubmitAlert(props:string) {
   const router = useRouter()
   return (
     <div className="fixed top-0 right-0 left-0 bg-slate-100/50 flex flex-col justify-center items-center w-screen h-screen backdrop-blur-sm">
-      <div className="text-2xl font-bold">✔{title}</div>
+      <div className="text-2xl font-bold">✔{props.title}</div>
       <button
         className="bg-blue-400 rounded-full px-4 text-white mt-4 p-2 mx-auto"
         onClick={() => router.push('/beatus')}
