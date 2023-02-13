e167acdab374c877e0ec32d9a4869ec4e4f483bd
Hyunwoo <88994338+jhyunwoo@users.noreply.github.com>
2023-02-13 16:01:08 +0900

    Update [...nextauth].ts

--- pages/api/auth/[...nextauth].ts
+++ pages/api/auth/[...nextauth].ts
@@ -24,7 +24,7 @@ export const authOptions = {
     }),
   ],
   callbacks: {
-    async session({ session }) {
+    async session({ session:any }) {
       let userData = await prisma.user.findUnique({
         where: {
           email: session.user.email,
